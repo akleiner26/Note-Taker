e2bb067453d56b8d963db2bc9a268419be10661d
orangeblack13 <AndrewJKleiner@gmail.com>
2020-07-05 15:08:55 -0400

    static express call added

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -9,6 +9,7 @@ var notes = [];
 //Express Setup
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 ////////////////////////////////////////////
 //Routes
 app.get("/notes", function(req, res){
